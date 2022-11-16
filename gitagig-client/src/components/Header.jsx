d1af682f45375ae4385617e2910b132aa96aae12
jeremyHarrell <hillvulf@gmail.com>
2022-11-16 13:51:55 -0600

    messed with conditional styling in Header

--- gitagig-client/src/components/Header.jsx
+++ gitagig-client/src/components/Header.jsx
@@ -5,8 +5,14 @@ import axios from 'axios'
 import Client from '../services/api'
 
 const Header = ({authenticated, user, handleLogOut}) => {
+  // const [isClicked, setIsClicked] = useState(false)
+
+  // const handleToggle = () => {
+  //   setIsClicked(!isClicked)
+  // }
+
   return (
-    <div className="nav-div">
+    <div className={authenticated ? "nav-div" : "loggedOutNavDiv"}>
       <h1>-HIRED-</h1>
       <h6>{authenticated ? <Link onClick={handleLogOut}to="/">LogOut</Link> : <Link to="/">Login</Link>} | <Link to="/register">Register</Link></h6>
       <h3> </h3>
