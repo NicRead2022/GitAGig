bee81ab158835d9ec5d48f462b1414058223c056
Jon Scarpelli <scarpelli.jon@gmail.com>
2022-12-20 10:41:03 -0600

    new base URL for non protected routes

--- src/components/Musicians.jsx
+++ src/components/Musicians.jsx
@@ -5,14 +5,14 @@ import axios from 'axios';
 import { useEffect } from 'react';
 
 
-const Musicians = ({selectedGig, bandleader}) => {
+const Musicians = ({ selectedGig, bandleader }) => {
   const navigate = useNavigate()
-  const initialState = {name: "", socialMedia: "", genre: "", about:"", image:""}
+  const initialState = { name: "", socialMedia: "", genre: "", about: "", image: "" }
   const [formState, setFormState] = useState(initialState)
-  const [musicians,setMusicians] = useState([])
+  const [musicians, setMusicians] = useState([])
 
   const getMusicians = async () => {
-    const res = await axios.get(`https://gitagig.herokuapp.com/api/musician`)
+    const res = await axios.get(`https://hired-serverside.herokuapp.com/api/musician`)
     setMusicians(res.data)
   }
 
@@ -21,12 +21,12 @@ const Musicians = ({selectedGig, bandleader}) => {
     getMusicians()
   }, [])
 
-const handleClick = async(e) => {
-  let musicianId= e.target.id
-  let addedGig = {gigId: selectedGig}
-  await axios.put(`https://gitagig.herokuapp.com/api/musician/${musicianId}`, addedGig )
-  navigate (`/bandleader/${bandleader.id}`)
-}
+  const handleClick = async (e) => {
+    let musicianId = e.target.id
+    let addedGig = { gigId: selectedGig }
+    await axios.put(`https://hired-serverside.herokuapp.com/api/musician/${musicianId}`, addedGig)
+    navigate(`/bandleader/${bandleader.id}`)
+  }
 
 
   return (
@@ -34,17 +34,17 @@ const handleClick = async(e) => {
       <div className='getMusicians'>
         {musicians && (musicians.map(musician => (
           <div className='musicians' key={musician.id}>
-            <img className="musician-img" alt="musician-img" src={musician.image}/>
+            <img className="musician-img" alt="musician-img" src={musician.image} />
             <h4 className="musician-name">{musician.name}</h4>
             <h5>-{musician.instrument}-</h5>
             <p className="musician-info"><b>preferred genre: </b> {musician.genre}</p>
             <p><b>about:</b> {musician.about}</p>
             <p><b>socials:</b> {musician.socialMedia}</p>
             <button id={musician.id} onClick={handleClick} className='add-musician-btn' >Add Musician</button>
-              </div>
+          </div>
         )))}
       </div>
-  </div>
+    </div>
   )
 }
 
--- src/components/RegisterMusician.jsx
+++ src/components/RegisterMusician.jsx
@@ -5,83 +5,83 @@ import { useNavigate } from 'react-router-dom';
 
 export const RegisterMusician = () => {
   const navigate = useNavigate()
-  const initialState = { name: "", instrument: "", socialMedia: "", image: "", genre:"", about:"" }
+  const initialState = { name: "", instrument: "", socialMedia: "", image: "", genre: "", about: "" }
   const [formState, setFormState] = useState(initialState)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    await axios.post(`https://gitagig.herokuapp.com/api/musician/`, formState )
+    await axios.post(`https://hired-serverside.herokuapp.com/api/musician/`, formState)
     setFormState(initialState);
     navigate('/')
   }
 
   const handleChange = (e) => {
-    setFormState({ ...formState, [e.target.name]: e.target.value});
+    setFormState({ ...formState, [e.target.name]: e.target.value });
   }
 
 
   return (
     <div className="registrationForm">
-                <h4 className="register-title">Add Yourself To Our List:</h4>
-    <form className="registrationInputContainer" onSubmit={handleSubmit}>
-    <div className='new-musician-input-wrapper'>
+      <h4 className="register-title">Add Yourself To Our List:</h4>
+      <form className="registrationInputContainer" onSubmit={handleSubmit}>
+        <div className='new-musician-input-wrapper'>
           <label className="registration-labels" htmlFor='name'>Name:</label>
           <input className="registration-inputs"
-             onChange={handleChange}
-             name="name"
-             type="text"
-             value={formState.name} required
+            onChange={handleChange}
+            name="name"
+            type="text"
+            value={formState.name} required
           />
         </div>
-    <div className='new-musician-input-wrapper'>
+        <div className='new-musician-input-wrapper'>
           <label className="registration-labels" htmlFor='instrument'>Instrument:</label>
           <input className="registration-inputs"
-             onChange={handleChange}
-             name="instrument"
-             type="text"
-             value={formState.instrument} required
+            onChange={handleChange}
+            name="instrument"
+            type="text"
+            value={formState.instrument} required
           />
         </div>
         <div className='new-musician-input-wrapper'>
           <label className="registration-labels" htmlFor='socialMedia'>Socials:</label>
           <input className="registration-inputs"
-             onChange={handleChange}
-             name="socialMedia"
-             type="text"
-             value={formState.socialMedia} 
+            onChange={handleChange}
+            name="socialMedia"
+            type="text"
+            value={formState.socialMedia}
           />
         </div>
         <div className='new-musician-input-wrapper'>
           <label className="registration-labels" htmlFor='genre'>Genre:</label>
           <input className="registration-inputs"
-             onChange={handleChange}
-             name="genre"
-             type="text"
-             value={formState.genre} 
+            onChange={handleChange}
+            name="genre"
+            type="text"
+            value={formState.genre}
           />
         </div>
         <div className='new-musician-input-wrapper'>
           <label className="registration-labels" htmlFor='about'>About:</label>
           <input className="registration-inputs"
-             onChange={handleChange}
-             name="about"
-             type="text"
-             value={formState.about} 
+            onChange={handleChange}
+            name="about"
+            type="text"
+            value={formState.about}
           />
         </div>
         <div className='new-musician-input-wrapper'>
           <label className="registration-labels" htmlFor='image'>Image:</label>
           <input className="registration-inputs"
-             onChange={handleChange}
-             name="image"
-             type="text"
-             value={formState.image} 
+            onChange={handleChange}
+            name="image"
+            type="text"
+            value={formState.image}
           />
         </div>
-      <button className="registerButton" type="submit">Create Musician</button>
-    </form>
+        <button className="registerButton" type="submit">Create Musician</button>
+      </form>
     </div>
-    
+
   )
 }
 
