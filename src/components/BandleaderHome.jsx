4872ab935c70655fcf49b6f64569a73df8acffff
Jon Scarpelli <scarpelli.jon@gmail.com>
2023-04-05 23:20:39 -0500

    musician card style fix

--- src/components/BandleaderHome.jsx
+++ src/components/BandleaderHome.jsx
@@ -4,54 +4,88 @@ import Client from '../services/api'
 import React from 'react'
 import { GigCard } from './GigCard'
 
-const BandleaderHome = ({selectedGig, setSelectedGig, selectedMusician, setSelectedMusician}) => {
-const [bandleaderDetails, setBandleaderDetails] = useState()
-const [bandleaderGigs, setBandleaderGigs] = useState([])
-const [deletedGig, toggleDeletedGig] = useState(false) 
+const BandleaderHome = ({
+  selectedGig,
+  setSelectedGig,
+  selectedMusician,
+  setSelectedMusician
+}) => {
+  const [bandleaderDetails, setBandleaderDetails] = useState()
+  const [bandleaderGigs, setBandleaderGigs] = useState([])
+  const [deletedGig, toggleDeletedGig] = useState(false)
 
-let {id}  = useParams()
+  let { id } = useParams()
 
-const getDetails = async () => {
-  const response = await Client.get(`https://hired-serverside.herokuapp.com/api/bandleader/${id}`) 
-  setBandleaderDetails(response.data[0])
-  let data = response.data
-  let bandGigs = []
-  data.forEach(element => {
-    bandGigs.push(element.Gigs) 
-  });
-  if (!bandGigs[0].id){
-    return null
-  } else
-  setBandleaderGigs(bandGigs)
-}
+  const getDetails = async () => {
+    const response = await Client.get(
+      `https://hired-serverside.herokuapp.com/api/bandleader/${id}`
+    )
+    setBandleaderDetails(response.data[0])
+    let data = response.data
+    let bandGigs = []
+    data.forEach((element) => {
+      bandGigs.push(element.Gigs)
+    })
+    if (!bandGigs[0].id) {
+      return null
+    } else setBandleaderGigs(bandGigs)
+  }
 
-useEffect(() => {
-      getDetails()
-}, [deletedGig])
+  useEffect(() => {
+    getDetails()
+  }, [deletedGig])
 
   return (
-    <div >
+    <div>
       <div className="bandleader-land">
-        <div className='bandleader-info'>
-        <h2 className="welcome">Welcome {bandleaderDetails?.name}!</h2>
-        <img className="profile-pic" alt="profile-pic" src={bandleaderDetails?.blImage}></img>
-        <h5>band name:{bandleaderDetails?.band}</h5>
-        <p>social media: <a href={`https://${bandleaderDetails?.socialMedia}`} rel="noreferrer" target="_blank">{bandleaderDetails?.socialMedia}</a></p>
+        <div className="bandleader-info">
+          <h2 className="welcome">Welcome {bandleaderDetails?.name}!</h2>
+          <img
+            className="profile-pic"
+            alt="profile-pic"
+            src={bandleaderDetails?.blImage}
+          ></img>
+          <h5>band name:{bandleaderDetails?.band}</h5>
+          <h6>
+            social media:{' '}
+            <a
+              href={`https://${bandleaderDetails?.socialMedia}`}
+              rel="noreferrer"
+              target="_blank"
+            >
+              {bandleaderDetails?.socialMedia}
+            </a>
+          </h6>
         </div>
-          <div className="gig-list">
-          <Link to='/new-gig'><button className='add-gigbtn'>Add Gig</button></Link>
+        <div className="gig-list">
+          <Link to="/new-gig">
+            <button className="add-gigbtn">Add Gig</button>
+          </Link>
         </div>
-          <div className="gigs-div">
+        <div className="gigs-div">
           <h4>Your Upcoming Gigs:</h4>
-                {!bandleaderGigs ? <h5>No Upcoming Gigs</h5> :           
-                bandleaderGigs.map((gig,index) => (
-                  <div key={gig.id} className="gig-card-wrapper">
-                    <GigCard key={gig.id} gigId={gig.id} selectedMusician={selectedMusician} setSelectedMusician={setSelectedMusician} selectedGig={selectedGig} setSelectedGig={setSelectedGig} deletedGig={deletedGig} toggleDeletedGig={toggleDeletedGig} setBandleaderGigs={setBandleaderGigs}/>
-                  </div>
-                 ))}
-            </div>
-          </div>
+          {!bandleaderGigs ? (
+            <h5>No Upcoming Gigs</h5>
+          ) : (
+            bandleaderGigs.map((gig, index) => (
+              <div key={gig.id} className="gig-card-wrapper">
+                <GigCard
+                  key={gig.id}
+                  gigId={gig.id}
+                  selectedMusician={selectedMusician}
+                  setSelectedMusician={setSelectedMusician}
+                  selectedGig={selectedGig}
+                  setSelectedGig={setSelectedGig}
+                  deletedGig={deletedGig}
+                  toggleDeletedGig={toggleDeletedGig}
+                  setBandleaderGigs={setBandleaderGigs}
+                />
+              </div>
+            ))
+          )}
+        </div>
       </div>
+    </div>
   )
 }
 
--- src/components/MusicianCard.jsx
+++ src/components/MusicianCard.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
-const MusicianCard = ({selectedMusician}) => {
-let {idx} = useParams()
-  return(
+const MusicianCard = ({ selectedMusician }) => {
+  let { idx } = useParams()
+  return (
     <div className="musician-card-container">
       <h3>{selectedMusician[idx].name}</h3>
-      <img className="musician-img" src={selectedMusician[idx].image}/>
+      <img className="musician-img" src={selectedMusician[idx].image} />
       <h5>-{selectedMusician[idx].instrument}-</h5>
-      <p>
-      <b>usually plays: </b> {selectedMusician[idx].genre}<br/><br/>
-      <b>about this musician:</b> {selectedMusician[idx].about}<br/><br/>
-      <b>find this musician at: </b>{selectedMusician[idx].socialMedia}</p>
+      <div>
+        <h6>genre: </h6> {selectedMusician[idx].genre}
+        <br />
+        <br />
+        <h6>about this musician:</h6> {selectedMusician[idx].about}
+        <br />
+        <br />
+        <h6>find this musician at: </h6>
+        {selectedMusician[idx].socialMedia}
+      </div>
     </div>
   )
 }
